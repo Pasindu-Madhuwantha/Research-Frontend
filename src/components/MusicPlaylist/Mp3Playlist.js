db607ad6147a51b2e81fe9e82ccf73921617de13
Pasindu Madhuwantha <73848766+Pasindu-Madhuwantha@users.noreply.github.com>
2024-05-11 18:17:20 +0530

    Update Mp3Playlist.js
    port change

--- src/components/MusicPlaylist/Mp3Playlist.js
+++ src/components/MusicPlaylist/Mp3Playlist.js
@@ -15,7 +15,7 @@ const MP3Playlist = () => {
 
   const fetchPlaylist = async () => {
     try {
-      const response = await fetch('http://localhost/api/playlist');      ;
+      const response = await fetch('http://localhost:8080/api/playlist');      ;
       const data = await response.json();
       setPlaylist(data.playlist);
     } catch (error) {
